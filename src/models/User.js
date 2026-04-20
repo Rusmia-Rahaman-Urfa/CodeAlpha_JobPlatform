7f57498aab6646bf7968dfc6bb736f66a2d4fb21
rusmiarahaman77461@gmail.com <rusmiarahaman77461@gmail.com>
2026-04-20 17:39:58 +0600

    refactor: add a logo, short and full description of companies

--- src/models/User.js
+++ src/models/User.js
@@ -4,6 +4,9 @@ const UserSchema = new mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     role: { type: String, enum: ['candidate', 'employer'], default: 'candidate' },
+    logo: { type: String, default: 'https://via.placeholder.com/150' }, // new add
+    shortDescription: { type: String }, // new add
+    fullDescription: { type: String }, // new add
     createdAt: { type: Date, default: Date.now }
 });
 
